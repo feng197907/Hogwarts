adaede301c52a3fd48f2e5f51f67902127d90963
wangtiefeng <274534017@qq.com>
2020-07-10 16:49:37 +0800

    修改conftest

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -2,14 +2,16 @@ import pytest
 from pyCode import calc
 
 
-@pytest.fixture(scope="function", autouse=True)
-def fixture_func():
-    print("开始计算")
-    yield
-    print("结束计算")
-
-
 @pytest.fixture()
 def cal_demo():
     c = calc.Calculator()
     return c
+
+@pytest.fixture()
+def fixture_func(cal_demo):
+    print("开始计算")
+    yield cal_demo
+    print("结束计算")
+
+
+
