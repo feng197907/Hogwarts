fa23423d644e8fb47ab06b80a935908f19eb98fe
wangtiefeng <274534017@qq.com>
2020-07-10 16:45:12 +0800

    修改case

--- testcases/test_func.py
+++ testcases/test_func.py
@@ -2,24 +2,27 @@ import pytest
 import allure
 import os
 
+test_data = ([(1, 1, 2), (2, 2, 4), (4, 8, 9)], [(10, 1, 9), (12, 2, 10), (14, 8, 9)],
+             [(3, 10, 30), (2, 8, 14), (4, 8, 30)], [(15, 3, 5), (20, 2, 10), (14, 7, 2)])
+
 
 class Test_Pytest():
 
-    @pytest.mark.parametrize('a,b,result', [(1, 1, 2), (2, 2, 4), (4, 8, 9)])
-    def test_add(self, cal_demo, a, b, result):
-        assert cal_demo.add(a, b) == result
+    @pytest.mark.parametrize('a,b,result', test_data[0])
+    def test_add(self, fixture_func, a, b, result):
+        assert fixture_func.add(a, b) == result
 
-    @pytest.mark.parametrize('a,b,result', [(10, 1, 9), (12, 2, 10), (14, 8, 9)])
-    def test_sub(self, cal_demo, a, b, result):
-        assert cal_demo.sub(a, b) == result
+    @pytest.mark.parametrize('a,b,result', test_data[1])
+    def test_sub(self, fixture_func, a, b, result):
+        assert fixture_func.sub(a, b) == result
 
-    @pytest.mark.parametrize('a,b,result', [(3, 10, 30), (2, 8, 14), (4, 8, 30)])
-    def test_mult(self, cal_demo, a, b, result):
-        assert cal_demo.mult(a, b) == result
+    @pytest.mark.parametrize('a,b,result', test_data[2])
+    def test_mult(self, fixture_func, a, b, result):
+        assert fixture_func.mult(a, b) == result
 
-    @pytest.mark.parametrize('a,b,result', [(15, 3, 5), (20, 2, 10), (14, 7, 2)])
-    def test_div(self, cal_demo, a, b, result):
-        assert cal_demo.div(a, b) == result
+    @pytest.mark.parametrize('a,b,result', test_data[3])
+    def test_div(self, fixture_func, a, b, result):
+        assert fixture_func.div(a, b) == result
 
 
 if __name__ == "__main__":
